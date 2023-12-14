4445897c4703e32f21ad55a5140f9d16bbd3b18a
beatriz.neves <beatriznevesporto@hotmail.com>
2023-12-14 10:02:28 -0300

    ajusta animação

--- Assets/VIDA_Enfermagem/Scripts/Quiz/AnswerScript.cs
+++ Assets/VIDA_Enfermagem/Scripts/Quiz/AnswerScript.cs
@@ -6,14 +6,17 @@ public class AnswerScript : MonoBehaviour
 {
     public string Value = "";
     private QuizManager QuizManager;
+    private GameObject Avatar;
 
     private void Start()
     {
         QuizManager = GameObject.FindWithTag("QuizManager").GetComponent<QuizManager>();
+        Avatar = GameObject.FindWithTag("Avatar");
     }
 
     public void Answer()
     {
+        Avatar.GetComponent<Animator>().Play("greeting", 0, 0.1f);
         QuizManager.AddAnswer(Value);
     }
 }
